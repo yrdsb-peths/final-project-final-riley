644dce4a18af83d2da19b2e0525affb0306dcc82
Pickle-Enjoyer <rileychoi20@gmail.com>
2025-05-21 08:27:28 -0400

    World

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,6 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        addObject(new Spaceship(), getWidth()/2, getHeight() - 50
     }
 }
